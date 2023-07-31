8f135f842675545bbc04b13b47e7b4f03b1030e4
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-31 14:34:33 +0530

    Added HTTP Feedback and notification when doing HTTP Request

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initalUiState = { cartIsVisible: false };
+const initalUiState = { cartIsVisible: false, notification: null };
 const uiSlice = createSlice({
   name: "ui",
   initialState: initalUiState,
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
