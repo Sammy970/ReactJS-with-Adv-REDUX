f732e0607f0a800faa76949fc2d3e52ffadae0ca
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-31 11:35:43 +0530

    Added Redux-toolkit and added cart-slice, ui-slice with redux-toolkit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initalUiState = { cartIsVisible: false };
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: initalUiState,
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
